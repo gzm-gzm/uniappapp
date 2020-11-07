3c0c3f620caac3f65f22eb48f77f35c0fb77fd38
PC-20190126TKXZ\Administrator <171683782@qq.com>
2020-11-07 12:05:15 +0800

    第一次提交

---
+++ uniapp-app/App.vue
@@ -0,0 +1,34 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			// 检测更新
+			//this.$U.update()
+			// 网络监听
+			this.$U.onNetWork()
+			// 初始化数据
+			// 初始化用户登录状态
+			this.$store.commit('initUser')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方css库 */
+	@import "./common/uni.css";
+	/* 自定义图标库 */
+	@import "./common/icon.css";
+	/* 动画库 */
+	@import "./common/animate.css";
+	/* 自定义样式库 */
+	@import "./common/free.css";
+	/* 全局样式 */
+	@import "./common/common.css";
+</style>
---
+++ uniapp-app/pages/user-list/user-list.vue
@@ -0,0 +1,193 @@
+<template>
+	<view>
+		<!-- tab -->
+		<view class="flex align-center" style="height: 100rpx;">
+			<view class="flex-1 flex align-center justify-center"
+			v-for="(item,index) in tabBars" :key="index"
+			:class="index === tabIndex ? 'font-lg font-weight-bold text-main':'font-md'"
+			@click="changeTab(index)">
+			{{item.name}} <text v-if="item.num > 0" class="ml-2">{{item.num|formatNum}}</text></view>
+		</view>
+		
+		<!-- 列表 -->
+		<swiper :duration="150" :current="tabIndex" @change="onChangeTab"
+		:style="'height:'+scrollH+'px;'">
+			<swiper-item v-for="(item,index) in newsList" :key="index">
+				<scroll-view scroll-y="true" :style="'height:'+scrollH+'px;'"
+				@scrolltolower="loadmore(index)">
+					<template v-if="item.list.length > 0">
+						<!-- 列表 -->
+						<block v-for="(item2,index2) in item.list" :key="index2">
+							
+							<!-- 列表样式 -->
+							<user-list :item="item2" :index="index"></user-list>
+							
+							
+						</block>
+						<!-- 上拉加载 -->
+						<load-more v-if="item.list.length > 10" :loadmore="item.loadmore"></load-more>
+					</template>
+					<!-- 无数据 -->
+					<template v-else>
+						<no-thing></no-thing>
+					</template>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
+		
+		
+	</view>
+</template>
+
+<script>
+	
+	import loadMore from '@/components/common/load-more.vue';
+	import noThing from '@/components/common/no-thing.vue';
+	import userList from '@/components/user-list/user-list.vue';
+	export default {
+		components: {
+			loadMore,
+			noThing,
+			userList
+		},
+		data() {
+			return {
+				// 列表高度
+				scrollH:600,
+				tabIndex:0,
+				tabBars:[{
+					name:"互关",
+					num:0,
+					key:"friends"
+				},{
+					name:"关注",
+					num:0,
+					key:"follows"
+				},{
+					name:"粉丝",
+					num:0,
+					key:"fens"
+				}],
+				
+				newsList:[]
+			}
+		},
+		// 监听点击输入框事件
+		onNavigationBarSearchInputClicked() {
+			uni.navigateTo({
+				url: '../search/search?type=user',
+			});
+		},
+		onNavigationBarButtonTap() {
+			uni.navigateBack({
+				delta: 1
+			});
+		},
+		onLoad() {
+			uni.getSystemInfo({
+				success:res=>{
+					this.scrollH = res.windowHeight - uni.upx2px(100)
+				}
+			})
+			// 根据选项生成列表
+			this.getData()
+		},
+		filters: {
+			formatNum(value) {
+				return value > 99 ? '99+' : value;
+			}
+		},
+		methods: {
+			// 获取用户相关数据
+			getCounts(){
+				this.$H.get('/user/getcounts/'+this.user.id,{},{
+					token:true,
+					notoast:true
+				}).then(res=>{
+					this.tabBars[0].num = res.friend_count
+					this.tabBars[1].num = res.withfollow_count
+					this.tabBars[2].num = res.withfen_count
+				})
+			},
+			// 获取数据
+			getData(){
+				var arr = []
+				for (let i = 0; i < this.tabBars.length; i++) {
+					// 生成列表模板
+					let obj = {
+						// 1.上拉加载更多  2.加载中... 3.没有更多了
+						loadmore:"上拉加载更多",
+						list:[],
+						page:1,
+						firstLoad:false
+					}
+					arr.push(obj)
+				}
+				this.newsList = arr
+				this.getList()
+			},
+			getList(){
+				let index = this.tabIndex
+				let id = this.tabBars[index].id
+				let page = this.newsList[index].page
+				let isrefresh = page === 1
+				this.$H.get('/'+this.tabBars[index].key+'/'+page,{},{
+					token:true,
+					noCheck:true
+				})
+				.then(res=>{
+					console.log(res);
+					let list = res.list.map(v=>{
+						return {
+							id:v.id,
+							avatar:v.userpic,
+							username:v.username,
+							sex:v.userinfo.sex,
+							age:v.userinfo.age,
+							isFollow:index !== 2
+						}
+					})
+			
+					this.newsList[index].list = isrefresh ? list : [...this.newsList[index].list,...list];
+					
+					this.newsList[index].loadmore  = list.length < 10 ? '没有更多了' : '上拉加载更多';
+					
+					if (isrefresh) {
+						this.newsList[index].firstLoad = true
+					}
+				}).catch(err=>{
+					if(!isrefresh){
+						this.newsList[index].page--;
+					}
+				})
+			},
+			// tab切换
+			changeTab(index){
+				this.tabIndex = index
+			},
+			// 监听滑动
+			onChangeTab(e){
+				this.changeTab(e.detail.current)
+				if(!this.newsList[e.detail.current].firstLoad){
+					this.getList()
+				}
+			},
+			// 上拉加载更多
+			loadmore(index){
+				// 拿到当前列表
+				let item = this.newsList[index]
+				// 判断是否处于可加载状态（上拉加载更多）
+				if (item.loadmore !== '上拉加载更多') return;
+				// 修改当前列表加载状态
+				item.loadmore = '加载中...'
+				// 数据请求
+				item.page++
+				this.getList()
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
